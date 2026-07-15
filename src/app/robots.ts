1681418773ff82e582a6ee63ad385f86bc2bac67
Muhammad Faizan Malik <faizanmm086@gmail.com>
2026-07-15 15:47:40 +0500

    added sitemaps

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'https://www.iptvdubai.com/sitemap.xml',
+    sitemap: 'https://iptvdubai.org/sitemap.xml',
   };
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://www.iptvdubai.com';
+  const baseUrl = 'https://iptvdubai.org';
 
   return [
     {
